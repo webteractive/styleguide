4fba9f85b1ab9914c8af4f6858ce3242905d3a66
Glen Bangkila <hadeinvane@gmail.com>
2019-02-19 13:42:01 +0800

    Update index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby TailwindCSS Starter',
+    title: 'Webteractive Style Guide',
   },
   plugins: [
     {
@@ -41,6 +41,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    'gatsby-plugin-offline',
   ],
 }
